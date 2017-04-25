b1bd1c3afc3d226847e62ff835e718f4f3143198
asreepada <sripada_ar@yahoo.co.in>
2017-04-25 12:57:31 -0400

    updated code

--- FirstNLastOccuranceOfKey.cpp
+++ FirstNLastOccuranceOfKey.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
 	//code
 	int A[] = {1, 3, 5, 5, 5, 5 ,67, 123, 125};
-	int first = 0;
-	int last = 0;
+	int first = -1;
+	int last = -1;
 	int len = sizeof(A)/sizeof(int);
 	cout<< len<<endl;
 	int key = 5;
 	for(int i=0;i<len;i++) {
 	    if(key==A[i]) {
-	    	if(first == 0)
+	    	if(first == -1)
 	    		first = i;
 	    	else 
 	    		last = i;
